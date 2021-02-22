de3d3589c9edade8449616e88b0ad946c5841174
Rachel Moser <moser.rachel@gmail.com>
2021-02-22 10:30:35 -0600

    Add feature tests for passenger and flights

---
+++ spec/features/creating_passengers_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature "Users can create passengers" do
+  let!(:chicago) { create(:airport, :chicago) }
+  let!(:new_york) { create(:airport, :new_york_city) }
+
+  let!(:ORD_NYC) { create(:tomorrow_morning_flight, id: 1, origin_airport: chicago, destination_airport: new_york) }
+
+  scenario "with valid attributes" do
+    visit "/bookings/new/?passenger_count=1&booking_option=1&commit=Book+Flight"
+    fill_in("booking_passengers_attributes_0_name", with: "Example Name")
+    fill_in("booking_passengers_attributes_0_email", with: "name@example.com")
+    click_on("Submit Booking")
+    within(".booking-results") do
+      expect(page).to have_content("Example Name")
+      expect(page).to have_content("NYC")
+    end
+  end
+
+  scenario "when providing invalid attributes" do
+    visit "/bookings/new/?passenger_count=1&booking_option=1&commit=Book+Flight"
+    fill_in("booking_passengers_attributes_0_name", with: "short")
+    fill_in("booking_passengers_attributes_0_email", with: "short.com")
+    click_on("Submit Booking")
+    within("form") do
+      expect(page).to have_content("Passengers name is too short")
+      expect(page).to have_content("Passengers email is too short")
+    end
+  end
+end
---
+++ spec/features/searching_flights_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature "Users can search flights" do
+  let!(:chicago) { create(:airport, :chicago, location: "Chicago, IL") }
+  let!(:new_york) { create(:airport, :new_york_city, location: "New York City, NY") }
+
+  let!(:ORD_NYC) { create(:tomorrow_morning_flight, id: 1, origin_airport: chicago, destination_airport: new_york) }
+
+  scenario "with valid selections" do
+    visit "/"
+    select("Chicago, IL", from: "origin_id")
+    select("New York City, NY", from: "destination_id")
+    select(2, from: "passenger_count")
+    fill_in("departure_date", with: Time.zone.tomorrow)
+    click_on("Find Flights")
+    within(".flight-results") do
+      expect(page).to have_content("Available Flights")
+      expect(page).to have_content("NYC")
+    end
+  end
+
+  scenario "with invalid matching origin and destination airports" do
+    visit "/"
+    select("Chicago, IL", from: "origin_id")
+    select("Chicago, IL", from: "destination_id")
+    select(2, from: "passenger_count")
+    fill_in("departure_date", with: Time.zone.tomorrow)
+    click_on("Find Flights")
+    within(".flash-alert") do
+      expect(page).to have_content("Please choose two different origin and destination locations!")
+    end
+  end
+end
