056e48a90da30180cd467aa4bfa0cf8c7b4e731b
Rachel Moser <moser.rachel@gmail.com>
2021-02-21 20:04:26 -0600

    Update seeds with more realistic flight durations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
@@ -13,8 +15,17 @@ def evening_time
   Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :evening)
 end
 
-def random_duration
-  rand(75..175)
+def find_flight_duration(airports)
+  {
+    SFO: { NYC: 308, ATL: 257, ORD: 224, LAX: 53, DFW: 181, MCO: 292, DEN: 125 },
+    NYC: { SFO: 308, ATL: 101, ORD: 99, LAX: 296, DFW: 173, MCO: 122, DEN: 199 },
+    ATL: { SFO: 257, NYC: 101, ORD: 84, LAX: 236, DFW: 98, MCO: 61, DEN: 151 },
+    ORD: { SFO: 224, NYC: 99, ATL: 84, LAX: 213, DFW: 106, MCO: 129, DEN: 116 },
+    LAX: { SFO: 53, NYC: 296, ATL: 236, ORD: 213, DFW: 155, MCO: 266, DEN: 113 },
+    DFW: { SFO: 181, NYC: 173, ATL: 98, ORD: 106, LAX: 155, MCO: 127, DEN: 88 },
+    MCO: { SFO: 292, NYC: 122, ATL: 61, ORD: 129, LAX: 266, DFW: 127, DEN: 190 },
+    DEN: { SFO: 125, NYC: 199, ATL: 151, ORD: 116, LAX: 113, DFW: 88, MCO: 190 }
+  }[airports[0].to_sym][airports[1].to_sym]
 end
 
 airports = [
@@ -34,13 +45,14 @@ finish = Time.zone.today + 1.week
 
 (start..finish).each do |date|
   airport_pairs.each do |airport_pair|
-    flight_duration = random_duration
+    flight_codes = airport_pair.map(&:code)
+    flight_duration = find_flight_duration(flight_codes)
 
     Flight.create!(origin_airport: airport_pair[0],
                    destination_airport: airport_pair[1],
                    departure_date: date,
                    departure_time: morning_time,
-                   duration: flight_duration + 5)
+                   duration: flight_duration)
 
     Flight.create!(origin_airport: airport_pair[0],
                    destination_airport: airport_pair[1],
@@ -52,14 +64,18 @@ finish = Time.zone.today + 1.week
                    destination_airport: airport_pair[1],
                    departure_date: date,
                    departure_time: evening_time,
-                   duration: flight_duration - 7)
+                   duration: flight_duration)
   end
 end
 
-first_booking = Booking.new(flight: Flight.first)
-second_booking = Booking.new(flight: Flight.last)
+earhart_booking = Booking.new
 
-Passenger.create!(name: "Amelia Earhart", email: "amelia@earhart.com", booking: first_booking)
-Passenger.create!(name: "Maude Bonney", email: "maude@bonney.com", booking: first_booking)
-Passenger.create!(name: "Urmila K. Parekh", email: "urmila@parekh.com", booking: second_booking)
-Passenger.create!(name: "Hermelinda Urvina", email: "hermelinda@urvina.com", booking: second_booking)
+Seat.new(flight: Flight.first, booking: earhart_booking)
+Seat.new(flight: Flight.first, booking: earhart_booking)
+Seat.new(flight: Flight.first, booking: earhart_booking)
+Seat.new(flight: Flight.first, booking: earhart_booking)
+
+Passenger.create!(name: "Amelia Earhart", email: "amelia@earhart.com", booking: earhart_booking)
+Passenger.create!(name: "Maude Bonney", email: "maude@bonney.com", booking: earhart_booking)
+Passenger.create!(name: "Urmila K. Parekh", email: "urmila@parekh.com", booking: earhart_booking)
+Passenger.create!(name: "Hermelinda Urvina", email: "hermelinda@urvina.com", booking: earhart_booking)
