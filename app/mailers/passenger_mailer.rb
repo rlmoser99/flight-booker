0e555b6fccff3ade5bddc1452aa7d7c56a8c498f
Rachel Moser <moser.rachel@gmail.com>
2021-04-02 15:12:05 -0500

    Add styling and update wording of mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -3,6 +3,6 @@ class PassengerMailer < ApplicationMailer
   def thank_you_email
     @user = params[:user]
     @booking = params[:booking]
-    mail to: @booking.passengers.pluck(:email), subject: "Your booking is confirmed"
+    mail to: @booking.passengers.pluck(:email), subject: "Flight Booker Confirmation"
   end
 end
